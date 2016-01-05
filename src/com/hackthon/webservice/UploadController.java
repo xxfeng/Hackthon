4c3c5ba1d78d5eb12cf9b1a2429b8480cc39ede3
Jessie Wei <whatjustxhink@gmail.com>
2016-01-05 11:30:19 +0800

    jiaxwei cleanup unnecessary code

--- src/com/hackthon/webservice/UploadController.java
+++ src/com/hackthon/webservice/UploadController.java
@@ -4,15 +4,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
-//import java.io.InputStream;
-//import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-//import org.glassfish.jersey.media.multipart.FormDataParam;
-//import java.io.BufferedOutputStream;
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.io.InputStream;
-//import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-//import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,7 +27,6 @@ public class UploadController {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
-//                ;
                 BufferedOutputStream stream = 
                         new BufferedOutputStream(new FileOutputStream(new File(name)));
                 stream.write(bytes);
@@ -52,75 +42,11 @@ public class UploadController {
             return new Greeting(String.format(template, str));
         }
     }
-	
-	
-	@RequestMapping(value="/upload2", method=RequestMethod.POST)
-	@ResponseBody
-    public String uploadFile(
-            @RequestParam("files") MultipartFile file) {
-//		,
-//        @RequestParam("filename") String name		
-//		if (!file.isEmpty()) {
-//            try {
-//                byte[] bytes = file.getBytes();
-//                BufferedOutputStream stream =
-//                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-//                stream.write(bytes);
-//                stream.close();
-//                return "You successfully uploaded " + name + "!";
-//            } catch (Exception e) {
-//                return "You failed to upload " + name + " => " + e.getMessage();
-//            }
-//        } else {
-//            return "You failed to upload " + name + " because the file was empty.";
-//        }
-		return "ok";
-    }
-//	@Path("fileUpload")
-//	 @POST
-//	    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//	    @Produces(MediaType.APPLICATION_JSON)
-//	@RequestMapping(value="/uploadFile", method=RequestMethod.POST)
-//	@ResponseBody
-//	    public String uploadFile2(
-//	            @FormDataParam("targetFiles") InputStream uploadedInputStream,
-//	            @FormDataParam("targetFiles") FormDataContentDisposition fileDetail,
-//	            @FormDataParam("path") String path) {
-//	        String uploadedFileFS = "D:\\workspace\\ShareProject\\upload\\"; 
-//	        String fileLocation = uploadedFileFS + fileDetail.getFileName();
-//	        writeToFile(uploadedInputStream, fileLocation);
-//	        String content = readFile(fileLocation);
-////	        String content2 = getStringFromInputStream(uploadedInputStream);
-//	        String output = "File uploaded to : " + content;// + uploadedFileLocation;
-//	        System.out.println(output);
-//	        String res = "{\"msg\":\""+ output +"\"}";
-//	        return Response.status(200).entity(res).build();
-////	        return this.buildResponse(res);
-//			return "ok";
-//
-//	    }
-	
+
 	@RequestMapping(value="/testPost", method=RequestMethod.POST)
 	@ResponseBody
     public String testPost(
             @RequestParam("params")String params) {
-//		,
-//        @RequestParam("filename") String name
-		
-//		if (!file.isEmpty()) {
-//            try {
-//                byte[] bytes = file.getBytes();
-//                BufferedOutputStream stream =
-//                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-//                stream.write(bytes);
-//                stream.close();
-//                return "You successfully uploaded " + name + "!";
-//            } catch (Exception e) {
-//                return "You failed to upload " + name + " => " + e.getMessage();
-//            }
-//        } else {
-//            return "You failed to upload " + name + " because the file was empty.";
-//        }
 		System.out.println("params = " + params);
 		return "ok";
     }
