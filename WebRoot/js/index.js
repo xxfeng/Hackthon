d8eae63e4fb4133a87f868c5c3214498e89bbbdf
Jessie Wei <whatjustxhink@gmail.com>
2016-01-05 14:01:06 +0800

    patch upload, modify position of js and html

--- WebRoot/js/index.js
+++ WebRoot/js/index.js
@@ -1,5 +1,44 @@
+$(document).ready(function(){
+	$("#sumitUploadFile").bind("click", uploadFile);
+});
 
-	
+function uploadFile() {
+    var fileObj = $("#selectedFile")[0].files[0];
+
+    var requestURL =  "REST/upload";
+    // FormData
+    var form = new FormData();
+    form.append("file", fileObj);
+    form.append("name", "hello");
+    var opts = {
+        url: requestURL,
+        data: form,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+//        headers: reqHeaders,
+        success: self.handleUploadFileSuccess,
+        error: self.handleUploadFileError
+    };
+
+    if(form.fake) {
+        // Make sure no text encoding stuff is done by xhr
+        opts.xhr = function() { var xhr = $.ajaxSettings.xhr(); xhr.send = xhr.sendAsBinary; return xhr; }
+        opts.contentType = "multipart/form-data; boundary="+form.boundary;
+        opts.data = form.toString();
+    }
+    $.ajax(opts);
+}
+
+self.handleUploadFileSuccess = function(data){
+	alert(data);
+}
+
+self.handleUploadFileError = function() {
+    alert("file upload failure");
+}
+
 	function recognize()
 	{
 		//var param=$("#text_area")[0].value;
--- src/com/hackthon/webservice/UploadController.java
+++ src/com/hackthon/webservice/UploadController.java
@@ -3,6 +3,12 @@ package com.hackthon.webservice;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,29 +22,42 @@ import com.hackthon.domain.Greeting;
 @Controller
 public class UploadController {
 	private static final String template = "Hello, %s!";
+	private static final String DATE_FORMAT = "yyyy-MM-dd";
+	private static final String FILE_ROOT = "d://uploadFile";
+//	@Context
+//    ServletContext sc;
+
 	@RequestMapping(value="/upload", method=RequestMethod.GET)
     public @ResponseBody String provideUploadInfo() {
         return "You can upload a file by posting to this same URL.";
     }
     
     @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody Greeting handleFileUpload(@RequestParam("name") String name, 
+    public @ResponseBody Greeting handleFileUpload(
             @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
+        	String name = file.getOriginalFilename();
+        	SimpleDateFormat dateformat = new SimpleDateFormat(DATE_FORMAT);
+        	String rootPath = FILE_ROOT + "//" + dateformat.format(new Date());
+        	File rootFile = new  File(rootPath);
+        	if(!rootFile.exists()){
+        		rootFile.mkdirs();
+        	}
+        	String location =  rootPath + "//" + name;
             try {
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream = 
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                        new BufferedOutputStream(new FileOutputStream(new File(location)));
                 stream.write(bytes);
                 stream.close();
-                String str =  "You successfully uploaded " + name + "!";
+                String str =  "file:" + name + " uploaded to location: "+ location;
                 return new Greeting(String.format(template, str));
             } catch (Exception e) {
                 String str =  "You failed to upload " + name + " => " + e.getMessage();
                 return new Greeting(String.format(template, str));
             }
         } else {
-            String str = "You failed to upload " + name + " because the file was empty.";
+            String str = "You failed to upload " + " because the file was empty.";
             return new Greeting(String.format(template, str));
         }
     }
@@ -51,5 +70,40 @@ public class UploadController {
 		return "ok";
     }
 	
-	
+//	private void writeToFile(OutputStream out,
+//            String uploadedFileLocation) {
+//        try {
+////            OutputStream out = new FileOutputStream(new File(
+////                    uploadedFileLocation));
+//            int read = 0;
+//            byte[] bytes = new byte[1024];
+//
+//            out = new FileOutputStream(new File(uploadedFileLocation));
+//            while ((read = uploadedInputStream.read(bytes)) != -1) {
+//                System.out.println("write file.....");
+//                out.write(bytes, 0, read);
+//            }
+//            out.flush();
+//            out.close();
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private String readFile(String path) {
+//        StringBuffer content = new StringBuffer();
+//        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+//            String sCurrentLine;
+//            while ((sCurrentLine = br.readLine()) != null) {
+//                System.out.println(sCurrentLine);
+//                content.append(sCurrentLine);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }finally{
+//            return content.toString();
+//        }
+//    }
+//
 }
